775ee675d4f6c6a818fc9297642eeb5e6013cb29
Arslan8106 <arslanahmad8106@gmail.com>
2022-08-22 16:35:33 +0500

    update2

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -63,14 +63,14 @@ class ArticlesController < ApplicationController
   def destroy
   
     @article.comments.ids.clear
-  
-    @article.user.user_id.clear  
+ 
+    @article.user.clear
           
     @article.destroy
     
 
     respond_to do |format|
-      format.html { redirect_to articles_url, notice: "Article was successfully destroyed." }
+      format.html { redirect_to articles_path, notice: "Article was successfully destroyed." }
       format.json { head :no_content }
     end
   end
